c1eed911deddb0e683e770eece19f0ec82f1c589
Steve-black <porndittha77@gmail.com>
2021-06-22 02:30:54 +0700

    Git init.

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: "l/bYa0rCwnTt+C1bffmn5jVGr3TLNhGdOJOd9l7d/2mdGjFaK9tyR3brrqLKo0r3AX/duIiyJ17Ioj0amJN0gyqdSuIlw9Mjx3/Ghn0SzQE5RTwGWkKWs3IBqwJu49cujEj3YNgnThnzmmAzW+/DUwdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "4a29127e9167281f3de947c921116f88",
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
